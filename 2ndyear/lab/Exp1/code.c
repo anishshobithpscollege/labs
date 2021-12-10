d47b53bbca68e9bdab65b095d1516b0e87db823f
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2021-12-10 20:08:44 +0530

    Update code.c

--- 2ndyear/lab/Exp1/code.c
+++ 2ndyear/lab/Exp1/code.c
@@ -1,12 +1,13 @@
 /**
  * Experiment - 1
  * 
- * Date: 25-10-2021
+ * Date: 29-10-2021
  * 
  * @AUTHOR     - Anish Shobith P S
  * @USN        - 4SO20CS012
  * @SUBJECT    - Data Structures Laboratory
  * @DEPARTMENT - Computer Science and Engineering
+ * @COLLEGE    - ST JOSEPH ENGINEERING COLLEGE Vamanjoor, Mangaluru
  * 
  * Question :
  * Design, Develop and Implement a menu driven Program in C for the following Array operations.
@@ -15,121 +16,117 @@
  *  c. Inserting an Element at given valid Position.
  *  d. Deleting an Element at a given valid Position.
  * Support the program with functions for each of the above operations.
- * 
- * Legends :
- *  Element = ELE
- *  Position = POS
  */
  
- #include <stdio.h>
- 
- int a[20], n, i, ele, pos;
- 
- void create();
- void display();
- void insert();
- void delete();
- 
- void main()
- {
-     int choice;
-     
-     while (1)
-     {
-         printf("\n");
-         printf("*****MENU*****");
-         printf("1. Create\n2. Display\n3. Insert\n4. Delete 5. Exit");
-         scanf("%d", &choice);
-     
-         
-         switch(choice)
-         {
-             case 1: create();
-                     break;
-            
-             case 2: display();
-                     break;
-            
-             case 3: insert();
-                     break;
-            
-             case 4: delete();
-                     break;
-                     
-             case 5: exit(0);
-                     break;
-                     
-             default: printf("\nInvalid Choice\n");
-                      break;
-         }
-     }
- }
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * Creates a Array (A) of size (N).
- * 
- * Algorithm:
- *  Setp 1 : SET N, size of the Array (A)
- * 
- *  Setp 2 : FOR I = 0 to I < N
- *              READ A[I]
- *           ENDFOR
- * 
- *  Setp 3 : EXIT
- */
+int a[20], n, i, ele, pos;
+
+void create();
+void display();
+void insert();
+void delete();
+
+void main()
+{
+	int choice;
+
+	while (1)
+	{
+		printf("\n");
+		printf("*****MENU*****\n");
+		printf("1. Create\n2. Display\n3. Insert\n4. Delete\n5. Exit");
+		printf("\n\tEnter you choice : ");
+		scanf("%d", &choice);
+
+		switch (choice)
+		{
+			case 1:
+				create();
+				break;
+
+			case 2:
+				display();
+				break;
+
+			case 3:
+				insert();
+				break;
+
+			case 4:
+				delete();
+				break;
+
+			case 5:
+				exit(0);
+				break;
+
+			default:
+				printf("\nInvalid Choice\n");
+				break;
+		}
+	}
+}
+
 void create()
 {
-    printf("Enter the size of the array : ");
-    scanf("%d", &n);
-    
-    printf("Enter the array elements : ");
-        for (i = 0; i < n; i++)
-            scanf("%d", a[i]);
+	printf("Enter the size of the array : ");
+	scanf("%d", &n);
+
+	printf("Enter the array elements : ");
+	for (i = 0; i < n; i++)
+		 scanf("%d", &a[i]);
 }
 
-/**
- * Display's elements in the Array (A).
- * 
- * Algorithm:
- *  Setp 1 : FOR I = 0 to I < N
- *              PRINT A[I]
- *           ENDFOR
- * 
- *  Setp 2 : EXIT
- */
 void display()
 {
-    printf("\n The Array elements are : ");
-        for (i = 0; i < n; i++)
-            printf("%d ", a[i]);
-    
+	printf("\n The Array elements are : ");
+	for (i = 0; i < n; i++)
+		 printf("%d ", a[i]);
+
 }
 
-/**
- * Insert's Element (ELE) into the Array (A).
- * 
- * Algorithm:
- *  Step 1 : SET POS
- * 
- *  Setp 2 : IF POS >= N + 1
- *              PRINT "Insertion is not possible."
- *           ELSE
- *           { 
- *              SET ELE
- *              FOR I = N - 1 to  I >= POS
- *                  A[N + 1] = A[I]
- *              ENDFOR
- *              A[I + 1] = A[I]
- *              A[POS] = ELE
- *              N = N + 1
- *           }
- * 
- *  Setp 3 : EXIT
- * 
- */ 
 void insert()
 {
-    printf("\n);
+	printf("Enter the position for the new element :");
+	scanf("%d", &pos);
+
+	if (pos >= n + 1)
+		printf("\nInsertion is not possible.\n");
+	else
+	{
+		printf("Enter element to be inserted : ");
+		scanf("%d", &ele);
+
+		for (i = n - 1; i >= pos; i--)
+			 a[i + 1] = a[i];
+
+		a[pos] = ele;
+		n = n + 1;
+	}
 }
- 
- 
+
+void delete()
+{
+	if (n == 0)
+		printf("\nNo elements to delete");
+	else
+	{
+		printf("Enter the postion of element to be deleted :");
+		scanf("%d", &pos);
+
+		if (pos >= n + 1)
+			printf("\nDeltetion is not possible");
+		else
+		{
+			ele = a[pos];
+
+			for (i = pos; i < n - 1; i++)
+				 a[i] = a[i + 1];
+
+			n = n - 1;
+			printf("The deleted element is %d", ele);
+		}
+	}
+}
