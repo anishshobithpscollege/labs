09dbb820632bc928a3d11a344400ba17204c03ea
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2021-12-10 20:22:47 +0530

    todo

---
+++ 2ndyear/lab/Exp2/code.c
@@ -0,0 +1,35 @@
+/**
+ * Experiment - 2
+ * 
+ * Date:  19-11-2021
+ * 
+ * @AUTHOR     - Anish Shobith P S
+ * @USN        - 4SO20CS012
+ * @SUBJECT    - Data Structures Laboratory
+ * @DEPARTMENT - Computer Science and Engineering
+ * @COLLEGE    - ST JOSEPH ENGINEERING COLLEGE Vamanjoor, Mangaluru 
+ * 
+ * Question :
+ * Design, Develop and Implement a Program in C for the following operations on Strings
+ * a. Read a main String (STR), a Pattern String (PAT) and a Replace String (REP)
+ * b. Perform Pattern Matching Operation: Find and Replace all occurrences of PAT in STR with
+ *    REP if PAT exists in STR. Report suitable messages in case PAT does not exist in STR
+ * Support the program with functions for each of the above operations. Don't use Built-in
+ * functions.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char STR[100], PAT[50], REP[50], RES[100];
+
+int pattern_match_replace()
+{
+  int i = 0;
+  int c = 0, m = 0;
+  int k = 0;
+  int flag = 0;
+  int j = 0;
+
+   
+}
