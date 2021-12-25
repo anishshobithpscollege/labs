84975e4bb2103fe972ba42a52ad4c640e2039931
Anish-Shobith <anish.shobith19@gmail.com>
2021-12-25 21:23:17 +0530

    feat(Exp): 5a and 5b
    Signed-off-by: Anish-Shobith <anish.shobith19@gmail.com>

---
+++ 2ndyear/lab/Exp5/Exp5b/code.c
@@ -0,0 +1,38 @@
+/**
+ * Experiment - 5a
+ *
+ * Date:  14-12-2021
+ *
+ * @AUTHOR     - Anish Shobith P S
+ * @USN        - 4SO20CS012
+ * @SUBJECT    - Data Structures Laboratory
+ * @DEPARTMENT - Computer Science and Engineering
+ * @COLLEGE    - ST JOSEPH ENGINEERING COLLEGE Vamanjoor, Mangaluru
+ *
+ * Question :
+ * Design, Develop and Implement a Program in C for the follwing Stack Applications
+ *  b. Solving Tower of Hanoie problem with n disks.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+void tower(int n, char source, char temp, char dest)
+{
+    if (n == 0)
+        return;
+    tower(n - 1, source, dest, temp);
+    printf("\nMove disc %d from %c to %c", n, source, dest);
+    tower(n - 1, temp, source, dest);
+}
+
+void main()
+{
+    int n;
+
+    printf("\nEnter the number of discs : ");
+    scanf("%d", &n);
+    tower(n, 'A', 'B', 'C');
+    printf("\n\n Total number of move are : %d ", (int)pow(2, n) - 1);
+}
+
