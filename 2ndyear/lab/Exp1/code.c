99120846d7a5ab4ce0e9345b375e86dbada49a76
Anish-Shobith <anish.shobith19@gmail.com>
2021-12-25 17:08:00 +0530

    chore(lint): Exp1
    Signed-off-by: Anish-Shobith <anish.shobith19@gmail.com>

--- 2ndyear/lab/Exp1/code.c
+++ 2ndyear/lab/Exp1/code.c
@@ -1,23 +1,23 @@
 /**
  * Experiment - 1
- * 
+ *
  * Date: 29-10-2021
- * 
+ *
  * @AUTHOR     - Anish Shobith P S
  * @USN        - 4SO20CS012
  * @SUBJECT    - Data Structures Laboratory
  * @DEPARTMENT - Computer Science and Engineering
  * @COLLEGE    - ST JOSEPH ENGINEERING COLLEGE Vamanjoor, Mangaluru
- * 
+ *
  * Question :
- * Design, Develop and Implement a menu driven Program in C for the following Array operations.
+ * Design, Develop and Implement a menu driven Program in C for the following Array  * operations.
  *  a. Creating an Arrat of N Integer Elements.
  *  b. Display of Array Elements with Suitable Headings.
  *  c. Inserting an Element at given valid Position.
  *  d. Deleting an Element at a given valid Position.
  * Support the program with functions for each of the above operations.
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -30,103 +30,103 @@ void delete();
 
 void main()
 {
-	int choice;
+    int choice;
 
-	while (1)
-	{
-		printf("\n");
-		printf("*****MENU*****\n");
-		printf("1. Create\n2. Display\n3. Insert\n4. Delete\n5. Exit");
-		printf("\n\tEnter you choice : ");
-		scanf("%d", &choice);
+    while (1)
+    {
+        printf("\n");
+        printf("*****MENU*****\n");
+        printf("1. Create\n2. Display\n3. Insert\n4. Delete\n5. Exit");
+        printf("\n\tEnter you choice : ");
+        scanf("%d", &choice);
 
-		switch (choice)
-		{
-			case 1:
-				create();
-				break;
+        switch (choice)
+        {
+        case 1:
+            create();
+            break;
 
-			case 2:
-				display();
-				break;
+        case 2:
+            display();
+            break;
 
-			case 3:
-				insert();
-				break;
+        case 3:
+            insert();
+            break;
 
-			case 4:
-				delete();
-				break;
+        case 4:
+            delete();
+            break;
 
-			case 5:
-				exit(0);
-				break;
+        case 5:
+            exit(0);
+            break;
 
-			default:
-				printf("\nInvalid Choice\n");
-				break;
-		}
-	}
+        default:
+            printf("\nInvalid Choice\n");
+            break;
+        }
+    }
 }
 
 void create()
 {
-	printf("Enter the size of the array : ");
-	scanf("%d", &n);
+    printf("Enter the size of the array : ");
+    scanf("%d", &n);
 
-	printf("Enter the array elements : ");
-	for (i = 0; i < n; i++)
-		 scanf("%d", &a[i]);
+    printf("Enter the array elements : ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
 }
 
 void display()
 {
-	printf("\n The Array elements are : ");
-	for (i = 0; i < n; i++)
-		 printf("%d ", a[i]);
+    printf("\n The Array elements are : ");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
 
 }
 
 void insert()
 {
-	printf("Enter the position for the new element :");
-	scanf("%d", &pos);
+    printf("Enter the position for the new element :");
+    scanf("%d", &pos);
 
-	if (pos >= n + 1)
-		printf("\nInsertion is not possible.\n");
-	else
-	{
-		printf("Enter element to be inserted : ");
-		scanf("%d", &ele);
+    if (pos >= n + 1)
+        printf("\nInsertion is not possible.\n");
+    else
+    {
+        printf("Enter element to be inserted : ");
+        scanf("%d", &ele);
 
-		for (i = n - 1; i >= pos; i--)
-			 a[i + 1] = a[i];
+        for (i = n - 1; i >= pos; i--)
+            a[i + 1] = a[i];
 
-		a[pos] = ele;
-		n = n + 1;
-	}
+        a[pos] = ele;
+        n = n + 1;
+    }
 }
 
 void delete()
 {
-	if (n == 0)
-		printf("\nNo elements to delete");
-	else
-	{
-		printf("Enter the postion of element to be deleted :");
-		scanf("%d", &pos);
+    if (n == 0)
+        printf("\nNo elements to delete");
+    else
+    {
+        printf("Enter the postion of element to be deleted :");
+        scanf("%d", &pos);
 
-		if (pos >= n + 1)
-			printf("\nDeltetion is not possible");
-		else
-		{
-			ele = a[pos];
+        if (pos >= n + 1)
+            printf("\nDeltetion is not possible");
+        else
+        {
+            ele = a[pos];
 
-			for (i = pos; i < n - 1; i++)
-				 a[i] = a[i + 1];
+            for (i = pos; i < n - 1; i++)
+                a[i] = a[i + 1];
 
-			n = n - 1;
-			printf("The deleted element is %d", ele);
-		}
-	}
+            n = n - 1;
+            printf("The deleted element is %d", ele);
+        }
+    }
 }
